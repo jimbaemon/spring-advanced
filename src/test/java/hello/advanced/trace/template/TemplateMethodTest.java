a738bf30d61559e42e70be68b714036872016bc6
jimbaemon <jimbae@kakao.com>
2022-02-10 00:08:29 +0900

    test: 템플릿 메서드 패턴을 구현한 케이스와 내부 클래스로 구현한 클래스 테스트 생성.

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -2,6 +2,9 @@ package hello.advanced.trace.template;
 
 import org.junit.jupiter.api.Test;
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -31,5 +34,40 @@ public class TemplateMethodTest {
 		long endTime = System.currentTimeMillis();
 		long resultTime = endTime - startTime;
 		log.info("resultTime={}", resultTime);
+	}
+
+	/**
+	 * 템플릿 메서드 패턴 적용
+	 */
+	@Test
+	void templateMethodV1(){
+		AbstractTemplate template1 = new SubClassLogic1();
+		template1.execute();
+		
+		AbstractTemplate template2 = new SubClassLogic2();
+		template2.execute();
+	}
+	
+	@Test
+	void templateMethodV2(){
+		AbstractTemplate template1 = new AbstractTemplate() {
+
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+		log.info("클래스 이름1={}", template1.getClass());
+		template1.execute();
+
+		AbstractTemplate template2 = new AbstractTemplate() {
+
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직2 실행");
+			}
+		};
+		log.info("클래스 이름2={}", template2.getClass());
+		template2.execute();
 	}
 }
